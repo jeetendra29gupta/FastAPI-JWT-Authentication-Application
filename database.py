f6630c99bdb34d5e0cd298c23582e27740e67074
Jeetendra Gupta <Jeetendra.rimt@gmail.com>
2024-10-25 10:00:41 +0530

    Update database.py
    removed unwanted things

--- database.py
+++ database.py
@@ -27,15 +27,3 @@ def insert_user(user_details: dict) -> int:
         int: The ID of the newly created user.
     """
     return db.insert(user_details)
-
-
-if __name__ == '__main__':
-    # output = db.search(User.username == 'juju_raven')
-    # print(output)
-
-    # output = db.get(User.username == 'juju_raven')
-    #print(output)
-    db.remove(User.username == 'test_user')
-    for item in db:
-        print(item)
-        db.remove(User.username == item['username'])
